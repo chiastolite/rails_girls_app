e252837ef05260240a59c91c9bb4ecbf069546c9
chiastolite <chiastolite.1980@gmail.com>
2013-12-31 04:00:14 +0900

    1.アプリケーションを作る

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Girls::Application.config.secret_key_base = '8d922fbcced3e41d0f70f84a3a4bceb86901c95963423d39d4406799799c1d88924e13dde23468525cf7e5913c037a92616b2d36cd846c5160d1d77686dfd0de'
