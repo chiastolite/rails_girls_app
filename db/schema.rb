8bace5263745194ae0a52969eb923e6c161ea289
chiastolite <chiastolite.1980@gmail.com>
2013-12-31 04:02:14 +0900

    2.Idea の scaffold をする

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131230190134) do
+
+  create_table "ideas", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "picture"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
